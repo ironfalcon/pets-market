92dda9e76c136e844e553813d8fdbbf1d07175d3
ironfalcon <ironfalcon@yandex.ru>
2021-02-10 00:57:04 +0400

    display pets and add link with router on diff page

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,17 +1,27 @@
 <template>
     <div>
-        <b-table striped hover :items="cats"></b-table>
+        <h1>Cats list</h1>
+        <b-table striped hover :items="cats">
+            <template #cell(name)="data">
+                <router-link :to="`/pets/cats/${data.index}`">
+                    {{ data.value }}
+                </router-link>
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script>
-import cats from '@/data/cats'
+import { mapState } from 'vuex'
 
 export default {
     data () {
-        return {
-            cats
-        }
+        return {}
+    },
+    computed: {
+        ...mapState([
+            'cats'
+        ])
     }
 }
 </script>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
-        Dogs
+        <h1>Dogs list</h1>
+        <b-table striped hover :items="dogs">
+            <template #cell(name)="data">
+                <router-link :to="`/pets/dogs/${data.index}`">
+                    {{ data.value }}
+                </router-link>
+            </template>
+        </b-table>
     </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    data () {
+        return {}
+    },
+    computed: {
+        ...mapState([
+            'dogs'
+        ])
+    }
+}
+</script>
